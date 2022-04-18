c564b0549df0b7369096342dd5f2fe793cd98cfa
Orangel Barrera <obarrerafranco@gmail.com>
2022-04-18 17:39:56 -0500

    Acceso con login y token

--- routes/api.php
+++ routes/api.php
@@ -6,8 +6,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts', PostV1::class)
-->only(['index','show','destroy']);
 
-Route::apiResource('v2/posts', PostV2::class)
-->only(['index','show']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('v1/posts', PostV1::class);
+    Route::apiResource('v2/posts', PostV2::class);
+});
+
+Route::post('login' , [
+App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
